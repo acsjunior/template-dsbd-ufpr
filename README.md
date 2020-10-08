49b6dd4882bb7f6a8aab6e26ae86057c1f091a7c
juniorssz <juniorssz@gmail.com>
2020-10-07 21:51:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# template-dsbd-ufpr
+
+Adaptação para o R do template disponibilizado pela UFPR para trabalho de conclusão do curso de Especialização em Data Science e Big Data.
+
+Template original: https://gitlab.c3sl.ufpr.br/dsbd/tcc
---
+++ scripts/install.R
@@ -0,0 +1,8 @@
+if (!require(pacman)) install.packages("pacman"); library(pacman)
+
+# Carrega os pacotes necessários
+# p_load(nome_do_pacote) para pacotes do CRAN
+# p_load_gh("nome_do_pacote") para pacotes do GitHub
+p_load(kableExtra) # pacote obrigatório, não excluir esta linha!!!
+p_load(tidyverse)
+
